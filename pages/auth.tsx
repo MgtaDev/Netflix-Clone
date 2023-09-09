79314cf5f74235dc895ab6372ba46aa4938a413f
danielpassantino <passantinodev@gmail.com>
2023-09-08 21:06:15 -0400

    day 2, auth, prisma, mongo, env

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,44 +1,67 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import Input from '@/components/input'
 import {useState} from 'react'
 
 const Auth = () => {
+
+//Login states
 const [email, setemail] = useState('')
 const [name, setname] = useState('')
 const [password, setpassword] = useState('')
 
 
+const [variant, setvariant] = useState('login')
+const toogleVariant = useCallback(()=>{
+    setvariant((currentVariant)=> currentVariant === 'login' ? 'register' : 'login'
+    )
+},[])
+
+
 return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-cover" >
-        <div className='bg-black bg-opacity-50 w-full h-full lg:bg-opacity-50'>
+        <div className='bg-black  w-full h-full lg:bg-opacity-50'>
             <nav className='px-12 py-5'>
                 <img src="/images/logo.png" alt="Logo" className='h-12' />
             </nav>
             <div className='flex justify-center'>
                 <div className='bg-black opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full'>
-                    <h2 className='text-white text-4xl mb-8 font-semibold'>Sign in</h2>
+                    <h2 className='text-white text-4xl mb-8 font-semibold'>{variant === 'login' ? 'Sign in' : 'Register'}</h2>
                     <div className='flex justify-center flex-col gap-4'>
+                        {variant === 'register' && (
+                                <Input 
+                                label='Username'
+                                onChange={(e: any)=> setname(e.target.value)}
+                                id='name'
+                                value={name}
+                                />
+                        )}
                         <Input 
                         label='Email'
-                        onChange={(e)=> setemail(e.target.value)}
+                        onChange={(e: any)=> setemail(e.target.value)}
                         id='email'
                         type='email'
                         value={email}
                         />
-                          <Input 
-                        label='Username'
-                        onChange={(e)=> setname(e.target.value)}
-                        id='name'
-                        value={name}
-                        />
                            <Input 
                         label='Password'
-                        onChange={(e)=> setpassword(e.target.value)}
+                        onChange={(e: any)=> setpassword(e.target.value)}
                         id='name'
                         type='password'
                         value={password}
                         />
                     </div>
+
+            <button className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition'>
+            {variant === 'login' ? 'Login' : 'Sign in'}
+            </button>
+
+            <p className='text-neutral-500 mt-12'>
+            {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
+            <span onClick={toogleVariant} className='text-white ml-3 hover:underline cursor-pointer'>
+            {variant === 'login' ? 'Create an account?' : 'Login'}
+            </span>
+            </p>
+
                 </div>
             </div>
         </div>
